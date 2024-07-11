ee9b8882790df224052d94b719795b113c98197a
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-07-11 10:44:14 +0900

    Feat: 리뷰 저장 컨트롤러 구현
    리뷰 저장 임시 컨트롤러 구현

---
+++ src/main/java/com/moview/controller/ReviewController.java
@@ -0,0 +1,58 @@
+package com.moview.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moview.model.dto.ImageRequestDTO;
+import com.moview.model.dto.ImageResponseDTO;
+import com.moview.model.entity.Member;
+import com.moview.model.entity.Review;
+import com.moview.model.entity.ReviewImage;
+import com.moview.service.ImageService;
+import com.moview.service.MemberService;
+import com.moview.service.ReviewService;
+import com.moview.service.S3Service;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ReviewController {
+
+	private final ImageService imageService;
+	private final S3Service s3Service;
+	private final ReviewService reviewService;
+	private final MemberService memberService;
+
+	@PostMapping("/review")
+	public ResponseEntity<ImageResponseDTO> ImagesUpload(@RequestBody ImageRequestDTO imageRequestDTO,
+		HttpSession httpSession) {
+
+		String email = (String)httpSession.getAttribute("email");
+		Member member = memberService.findByEmail(email);
+
+		Review review = Review.of(member, imageRequestDTO.getReviewTitle(), null);
+		Review createdReview = reviewService.save(review);
+
+		List<ReviewImage> reviewImages = s3Service.upload(imageRequestDTO.getImageFiles(), createdReview);
+		imageService.saveAll(reviewImages);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(new ImageResponseDTO(
+				createdReview.getId(),
+				reviewImages.stream()
+					.map(ReviewImage::getFileUrl)
+					.toList()
+			));
+
+	}
+
+}

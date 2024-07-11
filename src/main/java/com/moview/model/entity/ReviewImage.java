998b629f3ade30d338872934f6be94cb25217768
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-07-11 10:41:31 +0900

    Feat: 리뷰 이미지 엔티티 구현

---
+++ src/main/java/com/moview/model/entity/ReviewImage.java
@@ -0,0 +1,46 @@
+package com.moview.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "images")
+@ToString
+public class ReviewImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@ManyToOne
+	@JoinColumn(name = "review_id", nullable = false)
+	private Review review;
+
+	@Column(nullable = false, length = 1000)
+	private String fileName;
+
+	@Column(length = 1000)
+	private String fileUrl;
+
+	private ReviewImage(Review review, String type, String fileUrl) {
+		this.review = review;
+		this.fileName = type;
+		this.fileUrl = fileUrl;
+	}
+
+	public static ReviewImage of(Review review, String fileName, String fileURL) {
+		return new ReviewImage(review, fileName, fileURL);
+	}
+
+}

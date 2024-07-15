b539201c9981c35d5a40e789285d0a40b05a0a00
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-07-15 16:52:09 +0900

    Feat: 리뷰 요청 DTO 검증 추가, GlobalExceptionHandler 에러 처리 추가, 리펙토링
    리뷰 요청 DTO 기본 입력 검증 추가,
    GlobalExceptionHandler에 해당 에러처리 추가,
    IllegalStateException, IllegalArgumentException 에러처리 중복 코드 제거,
    error log 처리 기능 메서드 분리

--- src/main/java/com/moview/common/GlobalExceptionHandler.java
+++ src/main/java/com/moview/common/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.moview.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,16 +16,33 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(IllegalStateException.class) // 보통 커스텀 익셉션으로 사용
-	public ResponseEntity<String> handleGameIllegalStateException(IllegalStateException illegalStateException) {
-		log.info("error = {}", illegalStateException.getMessage());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(illegalStateException.getMessage());
+	@ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class}) // 보통 커스텀 익셉션으로 사용
+	public ResponseEntity<Map<String, String>> handleRuntimeExceptions(
+		RuntimeException runtimeException) {
+
+		logError(runtimeException);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(Map.of("message", runtimeException.getMessage()));
 	}
 
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<String> handleGameIllegalArgumentException(
-		IllegalArgumentException illegalArgumentException) {
-		log.info("error = {}", illegalArgumentException.getMessage());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(illegalArgumentException.getMessage());
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidExceptions(
+		MethodArgumentNotValidException methodArgumentNotValidException
+	) {
+
+		logError(methodArgumentNotValidException);
+
+		Map<String, String> errors = new HashMap<>();
+
+		methodArgumentNotValidException.getBindingResult()
+			.getAllErrors()
+			.forEach(error -> errors.put(((FieldError)error).getField(), error.getDefaultMessage()));
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
+
+	private static void logError(Exception exception) {
+		log.error("error = {} - {}", exception.getClass().getSimpleName(), exception.getMessage(), exception);
+	}
+
 }
--- src/main/java/com/moview/model/dto/request/ReviewRequestDTO.java
+++ src/main/java/com/moview/model/dto/request/ReviewRequestDTO.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,9 +20,16 @@ import lombok.ToString;
 @ToString
 public class ReviewRequestDTO {
 
+	@NotNull(message = "[ERROR] 제목은 null이 될 수 없습니다.")
+	@NotEmpty(message = "[ERROR] 제목을 입력해주세요")
 	private String title;
+
 	private List<MultipartFile> images;
+
+	@NotNull
+	@Size(min = 1, message = "[ERROR] 내용을 입력해주세요")
 	private List<String> texts;
+
 	private List<String> tags;
 
 }

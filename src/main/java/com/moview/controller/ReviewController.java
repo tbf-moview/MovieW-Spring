5f579315e6962391c01cf18dfc1852ab9b00df65
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-07-15 16:54:27 +0900

    Feat: review 조회 기능 구현, ReviewResponseDTO 구현
    리뷰 조회 기능 구현,
    리뷰 조회를 위한 ReviewResponseDTO 구현

--- src/main/java/com/moview/controller/ReviewController.java
+++ src/main/java/com/moview/controller/ReviewController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.moview.model.dto.request.ReviewRequestDTO;
+import com.moview.model.dto.response.ReviewResponseDTO;
 import com.moview.model.entity.Member;
 import com.moview.model.entity.Review;
 import com.moview.model.entity.ReviewImage;
@@ -37,7 +39,7 @@ public class ReviewController {
 	private final ReviewTagService reviewTagService;
 
 	@PostMapping("/review")
-	public ResponseEntity<String> createReview(@ModelAttribute ReviewRequestDTO reviewRequestDTO,
+	public ResponseEntity<String> createReview(@Validated @ModelAttribute ReviewRequestDTO reviewRequestDTO,
 		HttpSession httpSession) {
 
 		log.info("reviewRequestDTO : {}", reviewRequestDTO);
@@ -58,15 +60,23 @@ public class ReviewController {
 	}
 
 	@GetMapping("/review/{id}")
-	public ResponseEntity<?> findReview(@PathVariable(name = "id") Long id) {
+	public ResponseEntity<ReviewResponseDTO> findReview(@PathVariable(name = "id") Long id) {
 		Review review = reviewService.findByIdWithImagesAndTags(id);
 		log.info("review : {}", review);
-		return ResponseEntity.status(HttpStatus.OK).body(review);
+		return ResponseEntity.status(HttpStatus.OK).body(new ReviewResponseDTO(
+			review.getId(),
+			review.getTitle(),
+			review.getContent(),
+			review.getMember(),
+			review.getReviewTags(),
+			review.getCreateDate(),
+			review.getUpdateDate()
+		));
 	}
 
 	@PutMapping("/review/{id}")
 	public ResponseEntity<?> updateReview(@PathVariable(name = "id") Long id,
-		@ModelAttribute ReviewRequestDTO reviewRequestDTO) {
+		@Validated @ModelAttribute ReviewRequestDTO reviewRequestDTO) {
 
 		Review findReview = reviewService.findByIdWithImagesAndTags(id);
 		reviewImageService.deleteAll(findReview.getReviewImages());
---
+++ src/main/java/com/moview/model/dto/response/ReviewResponseDTO.java
@@ -0,0 +1,27 @@
+package com.moview.model.dto.response;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+import com.moview.model.entity.Member;
+import com.moview.model.entity.ReviewTag;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewResponseDTO {
+
+	private long id;
+	private String title;
+	private String content;
+	private Member member;
+	private Set<ReviewTag> reviewTags;
+	private Timestamp createDate;
+	private Timestamp updateDate;
+}
